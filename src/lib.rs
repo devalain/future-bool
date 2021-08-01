b7cd05cbb56b62793aab6b54842c420b8d3a0fbb
Alain Buisseret <me@alain.dev>
2021-08-01 22:22:24 +0200

    Added some doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,40 @@
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
 use tokio::sync::Notify;
-use std::sync::{Arc, atomic::{AtomicBool, Ordering}};
 
+/// A bool one can `await` the changes.
 #[derive(Clone)]
 pub struct FutureBool {
     notify_true: Arc<Notify>,
     notify_false: Arc<Notify>,
-    inner: Arc<AtomicBool>
+    inner: Arc<AtomicBool>,
 }
 
 impl FutureBool {
+    /// Creates a new `FutureBool` with some initial value.
     pub fn new(val: bool) -> Self {
         Self {
             notify_true: Arc::new(Notify::new()),
             notify_false: Arc::new(Notify::new()),
-            inner: Arc::new(AtomicBool::new(val))
+            inner: Arc::new(AtomicBool::new(val)),
         }
     }
+
+    /// Sets the `bool` value to `true`.
     pub fn set(&self) {
         self.inner.store(true, Ordering::Release);
         self.notify_true.notify_waiters();
     }
+
+    /// Sets the `bool` value to `false`.
     pub fn unset(&self) {
         self.inner.store(false, Ordering::Release);
         self.notify_false.notify_waiters();
     }
+
+    /// Returns the new value when it has changed.
     pub async fn wait_change(&self) -> bool {
         let val = self.inner.load(Ordering::Acquire);
         if val {
@@ -33,12 +44,16 @@ impl FutureBool {
         }
         !val
     }
+
+    /// If the value is `true`, returns immidiately, otherwise waits until it's `true`.
     pub async fn wait_true(&self) {
         let val = self.inner.load(Ordering::Acquire);
         if !val {
             self.notify_true.notified().await;
         }
     }
+
+    /// If the value is `false`, returns immidiately, otherwise waits until it's `false`.
     pub async fn wait_false(&self) {
         let val = self.inner.load(Ordering::Acquire);
         if val {
